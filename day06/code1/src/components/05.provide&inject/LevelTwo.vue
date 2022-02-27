456ed5076b78027ac4c47678a426c69c65d19f84
maizihao <pumpkin_mak@163.com>
2022-02-27 16:34:18 +0800

    20220227

---
+++ day06/code1/src/components/05.provide&inject/LevelTwo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h3>Level Two 二级组件</h3>
+
+    <hr />
+
+    <level-three></level-three>
+  </div>
+</template>
+
+<script>
+import LevelThree from './LevelThree.vue'
+// import { computed } from 'vue'
+
+export default {
+  name: 'LevelTwo',
+  // data() {
+  //   return {
+  //     color: 'blue',
+  //   }
+  // },
+  // provide() {
+  //   // 返回要共享的数据对象
+  //   return {
+  //     color: computed(() => this.color),
+  //     count: 2,
+  //   }
+  // },
+  components: {
+    LevelThree,
+  },
+}
+</script>
+
+<style lang="less" scoped></style>
