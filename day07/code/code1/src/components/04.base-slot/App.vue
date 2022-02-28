2c7f8cda2c55ab5c67e290712d6ddb5f8dcbba3b
maizihao <pumpkin_mak@163.com>
2022-03-01 00:56:52 +0800

    20220228

---
+++ day07/code/code1/src/components/04.base-slot/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>App 根组件</h1>
+    <hr />
+
+    <!-- 使用组件 -->
+    <my-com>
+      <p>这不是后备内容</p>
+    </my-com>
+  </div>
+</template>
+
+<script>
+// 导入组件
+import MyCom from './MyCom.vue'
+
+export default {
+  name: 'MyApp',
+  // 注册组件
+  components: {
+    MyCom,
+  },
+}
+</script>
+
+<style lang="less" scoped></style>
